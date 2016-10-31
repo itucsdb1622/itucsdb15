92c82864d0895284e4fc03107ff050ca1fec7b32
Kerim Yildirim <yildirimker@itu.edu.tr>
2016-11-01 01:36:11 +0200

    After that, user can register, login, update the user info of himself/herself, delete the account of himself/herself. Also i added some controls for managing user session as user wont see some things if he/she is not logged in.

--- server.py
+++ server.py
@@ -7,13 +7,16 @@ import psycopg2 as aligramdb
 from flask import Flask
 from flask import render_template
 from flask import redirect
+from flask import request
 from flask.helpers import url_for
+from flask import session
 from psycopg2.psycopg1 import connection, cursor
-
+import uuid
 
 
 app = Flask(__name__)
-
+app.config['SESSION_TYPE'] = 'aligram'
+app.config['SECRET_KEY'] = 'itucsdb1622'
 
 def get_elephantsql_dsn(vcap_services):
     """Returns the data source name for ElephantSQL."""
@@ -25,15 +28,144 @@ def get_elephantsql_dsn(vcap_services):
              dbname='{}'""".format(user, password, host, port, dbname)
     return dsn
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home_page():
+    with aligramdb.connect(app.config['dsn']) as connection:
+        cursor = connection.cursor()
+
+        query="""SELECT Username, Password FROM user_tb"""
+        cursor.execute(query)
+        data = cursor.fetchall()
+
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        exists = False
+        for row in data:
+            if username == row[0] and password == row[1]:
+                exists = True
+        
+        if exists == False:
+            error = 'Invalid Credentials. Please try again.'
+            session['loggedUser'] = None
+            session['loggedUserID'] = None
+            session['loginStatus'] = None
+        else:
+            with aligramdb.connect(app.config['dsn']) as connection:
+                cursor = connection.cursor()
+
+                cursor.execute("SELECT Id, Username FROM user_tb WHERE Username='%s'"%username)
+                data = cursor.fetchall()
+
+            user = data[0]
+            session['loggedUser'] = user[1]
+            session['loggedUserID'] = user[0]
+            session['loginStatus'] = 'OK'
+
+            return redirect(url_for('home_page'))
     now = datetime.datetime.now()
-    return render_template('home.html', current_time=now.ctime())
+    return render_template('home.html', error=error, session=session['loginStatus'], current_time=now.ctime())
 
 @app.route('/profile')
 def profile_page():
     now = datetime.datetime.now()
-    return render_template('profile_page.html', current_time=now.ctime())
+    return render_template('profile_page.html', session=session['loginStatus'], current_time=now.ctime())
+
+@app.route('/update_user', methods=['GET', 'POST'])
+def update_user():
+    with aligramdb.connect(app.config['dsn']) as connection:
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT Username FROM user_tb")
+        data = cursor.fetchall()
+
+    error = None
+    if request.method == 'POST':
+        new_username = request.form['username']
+        exists = False
+        for row in data:
+            if new_username == row[0]:
+                exists = True
+        
+        if exists == False:
+            with aligramdb.connect(app.config['dsn']) as connection:
+                cursor = connection.cursor()
+
+                cursor.execute("UPDATE user_tb SET Username='%s' WHERE ID='%s' "%(new_username, session['loggedUserID']))
+                session['loggedUser'] = new_username
+
+                return redirect(url_for('profile_page'))
+        else:
+            if new_username == session['loggedUser']:
+                error = 'This new username that you entered is the same as your current username'
+            else:
+                error = 'the new username already exists'
+    now = datetime.datetime.now()
+    return render_template('update_user.html', error=error, current_time=now.ctime())
+
+@app.route('/delete_user', methods=['GET', 'POST'])
+def delete_user():
+
+    delete_message = None
+    error = None
+    if request.method == 'POST':
+        with aligramdb.connect(app.config['dsn']) as connection:
+            if(request.form['id'] == session['loggedUserID']):
+                cursor = connection.cursor()
+
+                cursor.execute("DELETE FROM user_tb WHERE ID='%s' "%(session['loggedUserID']))
+                session['loggedUser'] = None
+                session['loggedUserID'] = None
+                session['loginStatus'] = None
+                return redirect(url_for('home_page'))
+            else:
+                error = 'You entered wrong id'
+
+    now = datetime.datetime.now()
+    return render_template('delete_user.html', error=error, session=session['loggedUserID'], current_time=now.ctime())
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    with aligramdb.connect(app.config['dsn']) as connection:
+        cursor = connection.cursor()
+
+        query="""SELECT Username, Password FROM user_tb"""
+        cursor.execute(query)
+        data = cursor.fetchall()
+
+    error = None
+    if request.method == 'POST':
+
+        username =  request.form['username'] 
+        password = request.form['password']
+
+        exists = False
+        for row in data:
+            if username == row[0] and password == row[1]:
+                exists = True
+        if exists:
+            error = 'Username is in use! Please choose another username'
+
+        else:
+        
+            with aligramdb.connect(app.config['dsn']) as connection:
+
+                cursor = connection.cursor()
+                query="""SELECT MAX(ID) FROM user_tb ID"""
+                cursor.execute(query)
+                data = cursor.fetchall()
+                counter = str(int(data[0][0]) + 1)
+
+                cursor.execute("INSERT INTO user_tb(ID, Username, Password) VALUES ('%s', '%s', '%s')"%(counter, username, password))
+                
+                connection.commit()
+
+                return redirect(url_for('home_page'))
+
+    return render_template('register.html', error=error)
 
 @app.route('/friends')
 def friends_page():
@@ -98,10 +230,10 @@ def create_table_for_user():
         query = """DROP TABLE IF EXISTS user_tb"""
         cursor.execute(query)
 
-        query="""CREATE TABLE user_tb(ID INTEGER NOT NULL,Firstname VARCHAR(40),Lastname VARCHAR(40),Age int,Gender VARCHAR(10),Email VARCHAR(100),PRIMARY KEY (ID))"""
+        query="""CREATE TABLE user_tb(ID VARCHAR(100) NOT NULL,Username VARCHAR(40), Password VARCHAR(10), Firstname VARCHAR(40),Lastname VARCHAR(40), Age int,Gender VARCHAR(10),Email VARCHAR(100), PRIMARY KEY (ID), UNIQUE(Username))"""
         cursor.execute(query)
 
-        query="""INSERT INTO user_tb(ID ,Firstname, Lastname) VALUES (1,'kerim','yildirim')"""
+        query="""INSERT INTO user_tb(ID ,Username, Password) VALUES (1,'kerim','test')"""
         cursor.execute(query)
 
         connection.commit()
