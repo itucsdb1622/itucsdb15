76c5bd4f3659732168f376b014420e90a495c374
umutyazgan <umut.yaz@gmail.com>
2016-12-30 04:27:59 +0200

    conf.py projenin son haline uyarlandı

--- docs/conf.py
+++ docs/conf.py
@@ -44,8 +44,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'itucsdb'
-copyright = '2015, Team Name'
+project = 'Aligram'
+copyright = '2016, itucsdb1622'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -197,8 +197,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'itucsdb.tex', 'itucsdb Documentation',
-   'Team Name', 'manual'),
+  ('index', 'Aligram.tex', 'Aligram Documentation',
+   'itucsdb1622', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -227,8 +227,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'itucsdb', 'itucsdb Documentation',
-     ['Team Name'], 1)
+    ('index', 'Aligram', 'Aligram Documentation',
+     ['itucsdb1622'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -241,8 +241,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'itucsdb', 'itucsdb Documentation',
-   'Team Name', 'itucsdb', 'One line description of project.',
+  ('index', 'Aligram', 'Aligram Documentation',
+   'itucsdb1622', 'Aligram', 'Aligram bir sosyal paylaşım platformudur.',
    'Miscellaneous'),
 ]
 
